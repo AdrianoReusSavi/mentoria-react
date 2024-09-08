d3c2506e2f94c4bd8dbe48ca4568a167d187b38e
adriano <adrianorsavi@gmail.com>
2024-09-08 18:51:54 -0300

    Ajustes em mensagem, layout e formulário

--- app/home.tsx
+++ app/home.tsx
@@ -1,8 +1,9 @@
 "use client";
 import React, { useState } from 'react';
 import { PlusOutlined, SearchOutlined } from '@ant-design/icons';
-import { Alert, Button, Checkbox, Col, Drawer, Form, Input, Pagination, Row, Space, Spin, Switch } from 'antd';
+import { Alert, Button, Checkbox, Col, Drawer, Form, Input, message, Pagination, Row, Space, Spin, Switch } from 'antd';
 import { useMutation, useQuery } from '@tanstack/react-query'
+import { NoticeType } from 'antd/es/message/interface';
 
 interface Todo {
   id: number;
@@ -17,7 +18,8 @@ const Home = () => {
   const [pageSize, setPageSize] = useState(10);
   const [title, setTitle] = useState('');
   const [searchTitle, setSearchTitle] = useState('');
-  const [newTodo, setNewTodo] = useState({ title: '', completed: false });
+  const [messageApi, contextHolder] = message.useMessage();
+  const [form] = Form.useForm();
 
   const showDrawer = () => {
     setOpen(true);
@@ -42,25 +44,25 @@ const Home = () => {
   }
 
   const handleSave = () => {
-    mutate(newTodo);
-    refetch();
-    onClose();
+    form.validateFields().then((values) => {
+      mutate(values);
+    });
   }
 
   const fetchTodos = async (page = 1, limit = 10, title = '') => {
     const response = await fetch(`https://jsonplaceholder.typicode.com/todos?_page=${page}&_limit=${limit}&q=${title}`);
     if (!response.ok) throw new Error('Network response was not ok');
     var data = await response.json();
-    var total = response.headers.get('x-total-count'); 
-    return { data , total: Number(total) };
+    var total = response.headers.get('x-total-count');
+    return { data, total: Number(total) };
   }
 
-  const { isLoading, error, data, isFetching, refetch } = useQuery({
+  const { isLoading, error, data, refetch } = useQuery({
     queryKey: ['todos', currentPage, pageSize, searchTitle],
     queryFn: () => fetchTodos(currentPage, pageSize, searchTitle),
     select: (response) => ({
-        todos: response.data,
-        total: response.total
+      todos: response.data,
+      total: response.total
     })
   });
 
@@ -79,80 +81,93 @@ const Home = () => {
   const { mutate, isPending } = useMutation({
     mutationFn: createTodo,
     onSuccess: () => {
-    }
-  })
+      refetch();
+      form.resetFields();
+      onClose();
+      showMessage('success', 'ToDo criado com sucesso!');
+    },
+    onError: () => {
+      showMessage('error', 'Erro ao criar ToDo!');
+    },
+  });
+
+  const showMessage = (tipo: NoticeType, mensagem: String) => {
+    messageApi.open({ type: tipo, content: mensagem, });
+  };
 
   if (isLoading) return <Spin />;
   if (error) return <Alert message="Error" description={error.message} type="error" />;
 
   return (
-      <div className="App">
-        <div className="div-header">
-          <div className="div-header-left">
-            <Input style={{ width: '85%' }} placeholder="Buscar por descrição" value={title} onChange={(e) => setTitle(e.target.value)} />
-            <Button type="primary" onClick={handleSearch}><SearchOutlined />Buscar</Button>
-          </div>
-          <div>
-            <Button type="primary" onClick={showDrawer} icon={<PlusOutlined />}>Novo ToDo</Button>
-            <Drawer
-              title="Novo ToDo"
-              width={600}
-              onClose={onClose}
-              open={open}
-              styles={{
-                body: {
-                  paddingBottom: 80,
-                },
-              }}
-              extra={
-                <Space>
-                  <Button onClick={onClose}>Cancelar</Button>
-                  <Button onClick={handleSave} type="primary" loading={isPending}>Salvar</Button>
-                </Space>
-              }
-            >
-              <Form layout="vertical" hideRequiredMark>
-                <Row gutter={16}>
-                  <Col span={12}>
-                    <Form.Item
-                      name="Descrição"
-                      label="Descrição"
-                      rules={[{ required: true, message: 'Informe a descrição' }]}
-                    >
-                      <Input placeholder="Informe a descrição" value={newTodo.title} onChange={(e) => setNewTodo({ ...newTodo, title: e.target.value })} />
-                    </Form.Item>
-                  </Col>
-                  <Col span={4}>
-                    <Form.Item
-                      name="Completed"
-                      label="Completo"
-                    >
-                      <Switch checked={newTodo.completed} onChange={(checked) => setNewTodo({ ...newTodo, completed: checked })} />
-                    </Form.Item>
-                  </Col>
-                </Row>
-              </Form>
-            </Drawer>
-          </div>
+    <div className="App">
+      <div className="div-header">
+        <div className="div-header-left">
+          <Input style={{ width: '85%' }} placeholder="Buscar por descrição" value={title} onChange={(e) => setTitle(e.target.value)} />
+          <Button type="primary" onClick={handleSearch}><SearchOutlined />Buscar</Button>
         </div>
-        <div className="div-body">
-          {data?.todos?.map((todo: Todo) => (
-            <Row justify="center" key={todo.id}>
-              <Col flex="30px"><Checkbox checked={todo.completed} disabled /></Col>
-              <Col flex="auto"><strong>{todo.title}</strong></Col>
-            </Row>
-          ))}
-          <Pagination
-            current={currentPage}
-            pageSize={pageSize}
-            total={data?.total || 0}
-            showSizeChanger
-            onChange={handlePageChange}
-            onShowSizeChange={handlePageSizeChange}
-            showTotal={(total, range) => `${range[0]}-${range[1]} de ${total} itens`}
-          />
+        <div>
+          <Button type="primary" onClick={showDrawer} icon={<PlusOutlined />}>Novo ToDo</Button>
+          <Drawer
+            title="Novo ToDo"
+            width={600}
+            onClose={onClose}
+            open={open}
+            styles={{
+              body: {
+                paddingBottom: 80,
+              },
+            }}
+            extra={
+              <Space>
+                <Button onClick={onClose}>Cancelar</Button>
+                {contextHolder}
+                <Button onClick={handleSave} type="primary" loading={isPending}>Salvar</Button>
+              </Space>
+            }
+          >
+            <Form form={form} layout="vertical">
+              <Row gutter={16}>
+                <Col span={12}>
+                  <Form.Item
+                    name="Title"
+                    label="Descrição"
+                    rules={[{ required: true, message: 'Informe a descrição' }]}
+                  >
+                    <Input placeholder="Informe a descrição" />
+                  </Form.Item>
+                </Col>
+                <Col span={4}>
+                  <Form.Item
+                    name="Completed"
+                    label="Completo"
+                  >
+                    <Switch />
+                  </Form.Item>
+                </Col>
+              </Row>
+            </Form>
+          </Drawer>
         </div>
       </div>
+      <div className="div-body">
+        {data?.todos?.map((todo: Todo) => (
+          <Row className="div-body-row" justify="center" key={todo.id}>
+            <Col className="div-body-col" flex="30px"><Checkbox checked={todo.completed} disabled /></Col>
+            <Col className="div-body-col" flex="auto"><strong>{todo.title}</strong></Col>
+          </Row>
+        ))}
+        <Pagination
+          className="div-body-pag"
+          current={currentPage}
+          pageSize={pageSize}
+          total={data?.total || 0}
+          showSizeChanger
+          onChange={handlePageChange}
+          onShowSizeChange={handlePageSizeChange}
+          showTotal={(total, range) => `${range[0]}-${range[1]} de ${total} itens`}
+        />
+      </div>
+    </div>
   );
 }
 
